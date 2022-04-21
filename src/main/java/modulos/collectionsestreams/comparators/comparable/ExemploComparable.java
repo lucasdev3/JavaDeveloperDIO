fcdc115bcda5a47606ff20b4972218d17dafc23c
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2022-04-21 01:06:53 -0300

    compparable init

---
+++ src/main/java/modulos/collectionsestreams/comparators/comparable/ExemploComparable.java
@@ -0,0 +1,12 @@
+package modulos.collectionsestreams.comparators.comparable;
+
+public class ExemploComparable {
+
+    public static void main(String[] args) {
+    /*
+    * Interface para definir regra de ordenação em uma classe de domínio.
+    * */
+
+
+    }
+}
---
+++ src/main/java/modulos/collectionsestreams/comparators/comparator/ExemploComparator.java
@@ -0,0 +1,11 @@
+package modulos.collectionsestreams.comparators.comparator;
+
+public class ExemploComparator {
+
+    public static void main(String[] args) {
+    /*
+    * Interface para definir classe com regra de ordenação.
+    * */
+
+    }
+}
